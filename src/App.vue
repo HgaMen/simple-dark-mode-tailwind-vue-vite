f9225a3ed618f723cd8fdc9af0b2b72959dbf462
HgaMen <hga.int2021@gmail.com>
2022-09-06 16:31:13 -0500

    data 1 - dark mode listo

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<script setup>
+import { ref } from 'vue';
+
+// On page load or when changing themes, best to add inline in `head` to avoid FOUC
+if (
+  localStorage.theme === 'dark' ||
+  (!('theme' in localStorage) &&
+    window.matchMedia('(prefers-color-scheme: dark)').matches)
+) {
+  document.documentElement.classList.add('dark');
+} else {
+  document.documentElement.classList.remove('dark');
+}
+
+// // Whenever the user explicitly chooses light mode
+// localStorage.theme = 'light'
+
+// // Whenever the user explicitly chooses dark mode
+// localStorage.theme = 'dark'
+
+// // Whenever the user explicitly chooses to respect the OS preference
+// localStorage.removeItem('theme')
+
+const toggleDarkMode = ref(document.documentElement.className === 'dark');
+
+const changeDarkMode = () => {
+  toggleDarkMode.value = document.documentElement.classList.toggle('dark');
+  toggleDarkMode.value
+    ? (localStorage.theme = 'dark')
+    : (localStorage.theme = 'light');
+};
+</script>
+
+<template>
+  <main class="container mx-auto text-center">
+    <h1 class="text-4xl dark:bg-gray-800 dark:text-white py-20">Dark mode</h1>
+
+    <button
+      class="bg-gray-700 text-white rounded-xl py-3 px-4"
+      @click="changeDarkMode"
+    >
+      Día/Noche {{ toggleDarkMode }}
+    </button>
+  </main>
+</template>
